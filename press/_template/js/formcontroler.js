b5a6f8ff481fdda5270d4229dfbb144802cf0beb
moonPress <studiomoonrise@gmail.com>
2017-05-03 15:50:12 -0300

    c02

--- press/_template/js/formcontroler.js
+++
@@ -1,7 +0,0 @@
-function Mudarestado(el) {
-    
-    var ele = document.getElementById(el);
-    var display = ele.style.display;
-    
-    ele.style.display = display == 'none' ? 'block' : 'none';
-}
